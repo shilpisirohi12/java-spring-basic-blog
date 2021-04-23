d39559338d2f96528b87932bac9d189a88bc6a46
Shilpi Sirohi <42121341+shilpisirohi12@users.noreply.github.com>
2021-04-22 23:06:14 -0400

    step 2

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,29 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
 
+    //@Autowired
+    private PostRepository postRepository;
+
+    public  BlogController(PostRepository postRepository){
+        this.postRepository=postRepository;
+    }
+
     @RequestMapping("/")
     public String listPosts(ModelMap model){
+        List<Post> posts = postRepository.getAllPosts();
         model.put("title","Blog Post 1");
+        model.put("posts",posts);
         return "home";
     }
 
